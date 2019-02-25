0d746a1c314c8e282052bccf10ca2dac58e4e976
lgxkevin <lgx9587@gmail.com>
2019-02-25 17:48:45 +1300

    change sendfile tp sendFile

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const Router = express.Router();
 
 Router.get('/add-product',(req,res,next)=>{
     //res.send('<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button><form></html>');
-    res.sendfile(path.join(__dirname,'../','views','add-product.html'))
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
 })
 
 Router.post('/add-product',(req,res,next)=>{
