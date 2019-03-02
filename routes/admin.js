4ddc8cae1539e6975abf015036ba6ed83a079869
lgxkevin <lgx9587@gmail.com>
2019-03-03 12:10:36 +1300

    add root directory

--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,10 @@ const express = require('express');
 
 const Router = express.Router();
 
+const rootDir = require ('../util/path');
 Router.get('/add-product',(req,res,next)=>{
     //res.send('<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button><form></html>');
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
 })
 
 Router.post('/add-product',(req,res,next)=>{
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,10 @@ const express = require('express');
 
 const Router = express.Router();
 
+const rootDir = require('../util/path');
 Router.get('/',(req,res,next)=>{
     // res.send('<h1>Hello from express</h1>');
-    res.sendFile(path.join(__dirname, '../','views', 'shop.html'));
+    res.sendFile(path.join(rootDir,'views', 'shop.html'));
 })
 
 module.exports = Router;
