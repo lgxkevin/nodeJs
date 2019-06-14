fb77b9225aacd1db34381cb1d0573abd18d649a7
lgxkevin <lgx9587@gmail.com>
2019-06-14 21:31:49 +1200

    refactor the file storage code

--- Models/product.js
+++ Models/product.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-const products = []
+const p = path.join(path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+
+}
 
 module.exports = class Product {
     constructor(title) {
@@ -10,34 +23,16 @@ module.exports = class Product {
 
     save() {
         // this will refer to the object created based on the class 
-        //products.push(this)
 
-        const p = path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
-        })
+        });
     }
     // static: call this method directly on the class itself and not on an instantiated object
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductFromFile(cb)
     }
 }
