9c5062ba89ede1965339185e4dc57ed756514b97
lgxkevin <lgx9587@gmail.com>
2019-09-16 12:10:22 +1200

    async client requst update

--- controllers/admin.js
+++ controllers/admin.js
@@ -164,8 +164,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -174,10 +174,10 @@ exports.postDeleteProduct = (req, res, next) => {
       fileHelper.deleteFile(product.imageUrl);
       return Product.deleteOne({ _id: prodId, userId: req.user._id })
     })
-    .then(result => res.redirect("/admin/products"))
+    .then(result => {
+      res.status(200).json({message:'Success!'});
+    })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({message: 'Error!'});
     });
 };
